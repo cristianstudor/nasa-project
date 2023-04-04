c1314a17b06991da0fd23567d01f54c5b09ce816
Cristian Tudor <cristi_tcs@live.com>
2023-04-04 23:51:23 +0300

    added MongoDB, mongoose to project, created launches and planets schemas

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const mongoose = require("mongoose");
 // For Creating multiple processes in Windows
 // const cluster = require("cluster");
 // cluster.schedulingPolicy = cluster.SCHED_RR;
@@ -9,10 +10,24 @@ const { loadPlanetsData } = require("./models/planets.model.js");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  "mongodb+srv://nasa-api:fCxj0lERXkCQbtb9@nasacluster.jsdipmj.mongodb.net/nasa_db?retryWrites=true&w=majority";
+
 const server = http.createServer(app);
 
+// Event emitter that emits events when the connection is ready
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
+
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
